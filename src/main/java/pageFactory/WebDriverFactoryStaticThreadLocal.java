6aff5e90748bf54bb54e373512300fcf4dc1997c
akakiy <ivachkevich63@gmail.com>
2022-06-12 18:49:46 +0300

    correct ThreadLocal

--- src/main/java/pageFactory/WebDriverFactoryStaticThreadLocal.java
+++ src/main/java/pageFactory/WebDriverFactoryStaticThreadLocal.java
@@ -1,6 +1,6 @@
 package pageFactory;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+//import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -8,7 +8,7 @@ public class WebDriverFactoryStaticThreadLocal {
     private static ThreadLocal<WebDriver> driver = new ThreadLocal<WebDriver>();
 
     public static  void setDriver() {
-        WebDriverManager.chromedriver().setup();
+//        WebDriverManager.chromedriver().setup();
         driver.set(new ChromeDriver());
     }
 
