1ab5c87773b80baad4f05fc6c9299ab679abbcc1
akakiy <ivachkevich63@gmail.com>
2022-06-12 16:51:35 +0300

    no start view

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,5 +1,7 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
@@ -9,6 +11,7 @@ import pageFactory.StartPage;
 import support.PropertiesReader;
 
 import static io.github.bonigarcia.wdm.WebDriverManager.chromedriver;
+import static io.github.bonigarcia.wdm.WebDriverManager.firefoxdriver;
 
 public class BaseTest {
     private WebDriver driver;
@@ -19,7 +22,9 @@ public class BaseTest {
 
     @BeforeMethod
     public void testSetUp(){
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless","--no-gpu");
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         driver.get(properties.getUrl());
     }
